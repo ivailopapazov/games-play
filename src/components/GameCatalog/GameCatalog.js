1583de821e3d660ec92ec2dd2594419796179671
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2021-11-12 20:40:22 +0200

    Show no game yes message

--- src/components/GameCatalog/GameCatalog.js
+++ src/components/GameCatalog/GameCatalog.js
@@ -8,20 +8,20 @@ const GameCatalog = () => {
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
-            .then(res => res.json())
-            .then(result => {
-                setGames(result)
-            });
+                .then(res => res.json())
+                .then(result => {
+                    setGames(result);
+                });
         }, 1000);
     }, []);
 
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-
-            { games.map(x => <GameCard game={x} />) }
-
-            <h3 className="no-articles">No games yet</h3>
+            { games.length > 0 
+                ? games.map(x => <GameCard key={x._id} game={x} />)
+                : <h3 className="no-articles">No games yet</h3>
+            }
         </section>
     );
 }
