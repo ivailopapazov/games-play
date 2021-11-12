f05888a6dea7cbacb2bb5a66faa001f89f28d57a
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2021-11-12 20:23:28 +0200

    Make a game catalog request

--- src/components/GameCatalog.js
+++ src/components/GameCatalog.js
@@ -1,4 +1,17 @@
+import { useEffect, useState } from 'react';
+
 const GameCatalog = () => {
+    const [games, setGames] = useState([]);
+    
+    useEffect(() => {
+        fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
+            .then(res => res.json())
+            .then(result => {
+                setGames(result)
+            });
+    }, []);
+
+
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
