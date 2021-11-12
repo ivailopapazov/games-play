4d8f2ad410894b9529f0d9033c0db4255a6c6164
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2021-11-12 19:39:03 +0200

    Add routes and navigation handler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import Header from './components/Header';
 import WelcomeWorld from './components/WelcomeWorld';
+import GameCatalog from './components/GameCatalog';
+import CreateGame from './components/CreateGame';
 
 function App() {
+    const routes = {
+        '/home': WelcomeWorld,
+        '/games': GameCatalog,
+        '/create-game':CreateGame,
+    };
+
     return (
         <div id="box">
             <Header />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,26 @@
 const Header = () => {
+    const onHeaderClick = (e) => {
+        e.preventDefault();
+
+        if (e.target.tagName == 'A') {
+            let url = new URL(e.target.href);
+
+            console.log(url.pathname);
+        }
+    };
+    
     return (
-        <header>
-            <h1><a className="home" href="#">GamesPlay</a></h1>
+        <header onClick={onHeaderClick}>
+            <h1><a className="home" href="/home">GamesPlay</a></h1>
             <nav>
-                <a href="#">All games</a>
+                <a href="/games">All games</a>
                 <div id="user">
-                    <a href="#">Create Game</a>
-                    <a href="#">Logout</a>
+                    <a href="/create-game">Create Game</a>
+                    <a href="/logout">Logout</a>
                 </div>
                 <div id="guest">
-                    <a href="#">Login</a>
-                    <a href="#">Register</a>
+                    <a href="/login">Login</a>
+                    <a href="/register">Register</a>
                 </div>
             </nav>
         </header>
